fc50e276c4ac058a8f5929489bf889560cbf8ce9
gisiona <gil-real@hotmail.com>
2020-04-12 01:12:19 -0300

    commit inicial

---
+++ vizi-frontend/src/components/Vizers.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Vizers extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Vizers</h2>
+        <hr />
+        <p>
+        Os Vizers darão suporte e apoio aos comerciantes no cadastro e divulgação dos negócios locais do bairro através da plataforma e com isso também fatura uma grana extra.
+        </p>
+        <a className="btn btn-warning btn-lg" href="#">
+          Saiba mais &raquo;
+        </a>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Vizers;
